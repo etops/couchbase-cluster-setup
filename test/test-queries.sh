a53f4c76b53d02dcfec8ddefda2f743dad467e2a
Moxious <moxious@oldhat.org>
2017-03-01 16:05:13 -0500

    test queries

---
+++ test/test-queries.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR=./test-queries
+AUTH="admin:admin001*"
+URL=http://localhost:8093/query/service
+
+curl -u $AUTH $URL -d "statement=select count(*) from models" | python -mjson.tool > $DIR/count-star.json
+curl -u $AUTH $URL -d "statement=select count(*) from models where _type='Portfolio'" | python -mjson.tool > $DIR/portfolios.json
+curl -u $AUTH $URL -d "statement=select * from system:indexes" | python -mjson.tool > $DIR/indexes.json
