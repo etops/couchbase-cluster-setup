2b23b45addbd01bdeefc8ae8806a1f3e9591f0d8
Christian Käslin <c.kaeslin@gmx.ch>
2017-10-15 17:07:34 +0200

    remove file bucket (#12)
    * remove file bucket
    
    * adjust test

--- test/test-queries.sh
+++ test/test-queries.sh
@@ -11,7 +11,7 @@ URL=http://localhost:8093/query/service
 curl -v -u $AUTH http://localhost:8091/pools | python -m json.tool > $DIR/pools.json
 curl -v -u $AUTH http://localhost:8091/pools/default/buckets | python -m json.tool > $DIR/buckets.json
 curl -v -u $AUTH http://localhost:8091/pools/default/buckets/models | python -m json.tool > $DIR/models.json
-curl -v -u $AUTH http://localhost:8091/pools/default/buckets/files | python -m json.tool > $DIR/files.json
+curl -v -u $AUTH http://localhost:8091/pools/default/buckets/rawdata | python -m json.tool > $DIR/rawdata.json
 curl -v -u $AUTH http://localhost:8092/models/_design/timeseries | python -mjson.tool > $DIR/timeseries_view.json
 curl -v $URL -d "$CREDS" -d "statement=select count(*) from models" | python -mjson.tool > $DIR/count-star.json
 curl -v $URL -d "$CREDS" -d "statement=select count(*) from models where _type='Portfolio'" | python -mjson.tool > $DIR/portfolios.json
