455aa877dcc12202300f6cd4304a9449e523f3e5
Christian Käslin <c.kaeslin@gmx.ch>
2020-01-09 16:17:31 +0100

    Update couchbase (#23)
    * remove automatic cluster configuration which pollutes the log
    
    * add circle ci 2 build config
    
    * update couchbase and remove all views
    
    * adjust to new backup restore tool
    
    * fix docker login
    
    * fix dockup
    
    * fix couchbase host
    
    * try to get local ip
    
    * try with hostname
    
    * fixing substitution
    
    * switch back
    
    * only first entry
    
    * create index before backup
    
    * fix create index statement
    
    * fix index names
    
    * adjust index creation
    
    * escape index creation and check for success
    
    * test
    
    * use different backup
    
    * switch back to hub docker, because of outgoing data limit of github
    
    * try backup with localhost
    
    * ip and port
    
    * use later version of dockup with disabled gsi indexes
    
    * fix test script creds
    
    * remove couchbase view and add build index after backup restore
    
    * check for 3 portfolios

--- test/test-queries.sh
+++ test/test-queries.sh
@@ -5,14 +5,13 @@ echo `date`
 
 DIR=./test-queries
 AUTH="$ADMIN_LOGIN:$ADMIN_PASSWORD"
-CREDS='creds=[{"user":"local:models","pass":"'$ADMIN_PASSWORD'"}]'
+CREDS='creds=[{"user":"'$ADMIN_LOGIN'","pass":"'$ADMIN_PASSWORD'"}]'
 URL=http://localhost:8093/query/service
 
 curl -v -u $AUTH http://localhost:8091/pools | python -m json.tool > $DIR/pools.json
 curl -v -u $AUTH http://localhost:8091/pools/default/buckets | python -m json.tool > $DIR/buckets.json
 curl -v -u $AUTH http://localhost:8091/pools/default/buckets/models | python -m json.tool > $DIR/models.json
 curl -v -u $AUTH http://localhost:8091/pools/default/buckets/rawdata | python -m json.tool > $DIR/rawdata.json
-curl -v -u $AUTH http://localhost:8092/models/_design/timeseries | python -mjson.tool > $DIR/timeseries_view.json
 curl -v $URL -d "$CREDS" -d "statement=select count(*) from models" | python -mjson.tool > $DIR/count-star.json
 curl -v $URL -d "$CREDS" -d "statement=select count(*) from models where _type='Portfolio'" | python -mjson.tool > $DIR/portfolios.json
 curl -v $URL -d "$CREDS" -d "statement=select * from system:indexes" | python -mjson.tool > $DIR/indexes.json
